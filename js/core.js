e6cd3a36b5c55638835e8224fcc0c03a91e7cbf7
Zhengru Jiang <jzr98@qq.com>
2017-06-30 17:21:19 +0800

    Finish the core function and many details. Plan to pause project because of time

--- js/core.js
+++ js/core.js
@@ -3,10 +3,10 @@ function Knowledge(){
 	this.ans = [];
 }
 
-var knowledges = [];
+var knowledges = [], historyrecode = [];
 var inputtext;
 var cnt = 0, MAX, MODE = 0, RandomMAX = 0; //MODE 0 English 1 Text 2 Ans Text
-var RandomMode = false; 
+var RandomMode = false, IsFinish = false; 
 var fileinput = document.getElementById('inputfile'),
 	view = document.getElementById('checkview'), //Console
 	ansinput = document.getElementById('input'),
@@ -33,7 +33,6 @@ function ChooseCase2(){
   MODE = 1;
   RandomMode = false; 
   view.value = "You have choosen Text Mode.\nWaiting for file selection...\n";
-  display.value = "";
 }
 
 function ChooseCase3(){
@@ -45,21 +44,35 @@ function ChooseCase3(){
 
 function hotkey(){
 	var a = window.event.keyCode;
-	if(a === 73 && event.ctrlKey){
-		if(MODE === 0){
-			Getcnt();
-			Show();
+	if(a === 73 && event.ctrlKey){	// ctrl + i
+		Getcnt();
+		Show();
+	}
+	else if(a === 27){	// ecs
+		
+		if(RandomMode){
+			if(historyrecode.length > 0){
+				knowledges.push(historyrecode.pop());
+				RandomMAX++;
+				cnt = knowledges.length - 1;
+			}
 		}
+		else{
+			if(cnt > 0)cnt--;	
+		}
+		Show();
 	}
 	else if(a === 85 && event.ctrlKey && event.altKey){
 		RandomMAX = MAX;
 		if(!RandomMode){
-			view.value += "Random Mode\n";
+			view.value += "\nRandom Mode\n";
 			RandomMode = true;
+			view.scrollTop = view.scrollHeight;
 		}
 		else{
-			view.value += "Normal Mode\n";
+			view.value += "\nNormal Mode\n";
 			RandomMode = false;
+			view.scrollTop = view.scrollHeight;
 		}
 	}
 }
@@ -73,6 +86,7 @@ function Getcnt(){
 	else{
 		if(RandomMAX > 0){
 			if(cnt != -1){
+				historyrecode.push(knowledges[cnt]);
 				knowledges.splice(cnt, 1);
 				RandomMAX--;
 			}
@@ -87,6 +101,27 @@ function Getcnt(){
 function Check(){
 	var ans = "";
 	var t = 0;
+
+	if(IsFinish && ansinput.value == 'r'){
+		var file = fileinput.files[0];
+		var reader = new FileReader();
+
+		if(RandomMode)
+			RandomMAX = MAX;
+
+		reader.onload = function(){
+			inputtext = this.result;
+			
+			if(MODE == 0)InitCase1();
+    		else if(MODE == 1)InitCase2();
+    		else if(MODE == 2)InitCase3();
+		};
+		reader.readAsText(file);
+		
+		ansinput.value = '';
+		
+		return;
+	}
 	
 	if(MODE == 0){
 		for(var i in knowledges[cnt].ans){
@@ -98,7 +133,7 @@ function Check(){
 	
 	
 		if(ansinput.value == ans){
-			view.value += (cnt + 1) + ".Correct\n";
+			view.value += (cnt + 1) + " " + display.value + " " + ans + "\tCorrect\n";
 			ansinput.value = '';
 			Getcnt();
 			Show();
@@ -124,9 +159,11 @@ fileinput.addEventListener('change', function Display(){
 	var reader = new FileReader();
 	reader.onload = function(){
 		inputtext = this.result;
-		if(MODE == 0)view.value += "Input finished...\nPress:\n\tctrl+alt+u to random mode (or switch normal mode)\n\tctrl+i to skip\n\tctrl+o to return\n";
-    	else if(MODE == 1)view.value += "Input finished...\nPress:\n\tctrl+alt+u to random mode (or switch normal mode)\n\tAny input to skip\n\tctrl+o to return\n";
-    	else if(MODE == 2)view.value += "Input finished...\nPress:\n\tctrl+alt+u to random mode (or switch normal mode)\n\ti input to display answer, any other input to skip\n\tctrl+o to return\n";
+		if(MODE == 0)view.value += "Input finished...\nPress:\n\tctrl+alt+u to random mode (or switch normal mode)\n\tctrl+i to skip\n\tEsc to return\n";
+    	else if(MODE == 1)view.value += "Input finished...\nPress:\n\tctrl+alt+u to random mode (or switch normal mode)\n\tAny input to skip\n\tEsc to return\n";
+    	else if(MODE == 2)view.value += "Input finished...\nPress:\n\tctrl+alt+u to random mode (or switch normal mode)\n\ti input to display answer, any other input to skip\n\tEsc to return\n";
+		
+		view.scrollTop = view.scrollHeight;
 		
 		if(MODE == 0)InitCase1();
     	else if(MODE == 1)InitCase2();
@@ -136,11 +173,17 @@ fileinput.addEventListener('change', function Display(){
 
 });
 
+function ParameterReset(){
+	knowledges.splice(0, knowledges.length);
+	historyrecode.splice(0, historyrecode.length);
+	IsFinish = false;
+}
+
 function InitCase1(){
 	var data = inputtext;
 	var mode = 0, k = 0;
 
- 	knowledges.splice(0, knowledges.lenth);
+ 	ParameterReset();
 	knowledges.push(new Knowledge());
 
 	for(var i in data){
@@ -174,7 +217,7 @@ function InitCase2(){
   	var data = inputtext;
   	var mode = 0, k = 0;
   
-  	knowledges.splice(0, knowledges.lenth);
+  	ParameterReset();
   	knowledges.push(new Knowledge());
   
   	for(var i = 0; i < data.length; i++){
@@ -212,7 +255,7 @@ function InitCase3(){
   	var data = inputtext;
   	var mode = 0, k = 0, state = 0;
   
-  	knowledges.splice(0, knowledges.lenth);
+  	ParameterReset();
   	knowledges.push(new Knowledge());
 	
   	for(var i = 0; i < data.length; i++){
@@ -268,7 +311,8 @@ function Show(){
 	display.value = '';
 	
 	if(cnt > MAX){
-		view.value += "\nCongratulations, you have finished this text!\n";
+		IsFinish = true;
+		view.value += "\n\nCongratulations, you have finished this text!\n\nInput r to restart.\n";
 		view.scrollTop = view.scrollHeight;
 		return;
 	}
